cc706d3720457eb0c2275a0de7d704ca1a1dbbc3
Samy Khelfa <samy@Samys-MacBook-Air.local>
2025-03-09 16:44:03 +0100

    feat: Implement event reservation system with automated notifications

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class ReservationsService {
@@ -50,13 +51,50 @@ export class ReservationsService {
     });
 
     if (!reservation) {
-      throw new Error('Aucune reservation trouvée pour cet évènement');
+      throw new Error('Aucune réservation trouvée pour cet événement.');
     }
 
     await this.prisma.reservation.delete({
       where: { userId_eventId: { userId, eventId } },
     });
 
+    const event = await this.prisma.event.findUnique({
+      where: { id: eventId },
+      include: { users: true },
+    });
+
+    if (event && event.users.length < event.maxParticipants) {
+      const availableUsers = await this.prisma.user.findMany({
+        where: {
+          id: { notIn: event.users.map((user) => user.userId) },
+        },
+      });
+
+      availableUsers.forEach((user) => {
+        this.sendNotification(user.email, event.name);
+      });
+    }
+
     return { message: 'Votre réservation a bien été annulée.' };
+  }
+
+  async sendNotification(email: string, eventName: string) {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Une place s’est libérée !',
+      text: `Une place s'est libérée pour l'événement ${eventName} ! Réservez vite avant qu'il ne soit complet !`,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log(`📩 Email envoyé à ${email} !`);
   }
 }
